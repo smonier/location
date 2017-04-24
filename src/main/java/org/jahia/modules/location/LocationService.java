a6f84964bfd4f41714193ec914ded25bca0c6b5d
Sergiy Shyrkov <sshyrkov@jahia.com>
2017-04-24 13:51:00 +0200

    [QA-9408] Added new property (j:geocodeAutomatically) which controls if geocoding should be done automatically or not

--- src/main/java/org/jahia/modules/location/LocationService.java
+++ src/main/java/org/jahia/modules/location/LocationService.java
@@ -61,8 +61,10 @@ public class LocationService {
         final Geocoder geocoder = new Geocoder();
         JCRNodeWrapper nodeWrapper = node.getNode();
 
-        StringBuffer address = new StringBuffer();
-        address.append(nodeWrapper.getProperty("j:street").getString());
+        StringBuilder address = new StringBuilder();
+        if (nodeWrapper.hasProperty("j:street")) {
+            address.append(nodeWrapper.getProperty("j:street").getString());
+        }
         if (nodeWrapper.hasProperty("j:zipCode")) {
             address.append(" ").append(nodeWrapper.getProperty("j:zipCode").getString());
         }
@@ -75,12 +77,14 @@ public class LocationService {
         if (!nodeWrapper.isNodeType("jnt:location") && !nodeWrapper.isNodeType("jmix:geotagged")) {
             nodeWrapper.addMixin("jmix:geotagged");
         }
-        GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(address.toString()).getGeocoderRequest();
-        GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
-        List<GeocoderResult> results = geocoderResponse.getResults();
-        if (results.size() > 0) {
-            nodeWrapper.setProperty("j:latitude", results.get(0).getGeometry().getLocation().getLat().toString());
-            nodeWrapper.setProperty("j:longitude", results.get(0).getGeometry().getLocation().getLng().toString());
+        if (address.length() > 0) {
+            GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(address.toString()).getGeocoderRequest();
+            GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+            List<GeocoderResult> results = geocoderResponse.getResults();
+            if (results.size() > 0) {
+                nodeWrapper.setProperty("j:latitude", results.get(0).getGeometry().getLocation().getLat().toString());
+                nodeWrapper.setProperty("j:longitude", results.get(0).getGeometry().getLocation().getLng().toString());
+            }
         }
     }
 }
--- src/main/resources/META-INF/rules.dsl
+++ src/main/resources/META-INF/rules.dsl
@@ -1,1 +1,3 @@
+[condition][]Location data has been changed on a node=property : ChangedPropertyFact ( name in ("j:street", "j:zipCode", "j:town", "j:country", "j:geocodeAutomatically"), propertyName : name, propertyValue : stringValues , node : node , $node : node, node.types contains "jnt:location" || node.types contains "jmix:locationAware")
+[condition][]Automatic geocoding is enabled on the node=ChangedPropertyFact ( name == "j:geocodeAutomatically" , stringValue == "true" ) from $node.properties
 [consequence][]Geocode the {node}=locationService.geocodeLocation({node}, drools);
